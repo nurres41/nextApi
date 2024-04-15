8a5e7c915a5a95a4bfba7534655824153e763c66
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-15 22:43:34 +0200

    MongoDB connetion done

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,15 +1,26 @@
-function handler(req, res) {
-    if(req.method === 'POST') {
-        const userEmail = req.body.email;
+import { MongoClient } from "mongodb";
 
-        if(!userEmail || !userEmail.includes('@')) {
-            res.status(422).json({ message: 'Invalid input. '})
-            return;
-        }
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
 
-        console.log(userEmail)
-        res.status(201).json({ message: "Signed up!"})
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid input. " });
+      return;
     }
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://nurionurkurtulus:nuri4141@cluster0.tptu29r.mongodb.net/newsletter?retryWrites=true&w=majority&appName=Cluster0"
+    );
+
+    const db = client.db();
+
+    await db.collection("emails").insertOne({ email: userEmail });
+
+    client.close()
+
+    res.status(201).json({ message: "Signed up!" });
+  }
 }
 
 export default handler;
