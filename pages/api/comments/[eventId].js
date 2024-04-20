14840145f6a8ea719ac6c1d6b7e1f5221ea07bd3
nurionurkurtulus <nurionurkurtulus@gmail.com>
2024-04-20 21:27:42 +0200

    EventId refactor

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -1,34 +1,62 @@
+import mongoDb from "../../../helpers/db";
 
-function handler(req, res) {
-    const eventId = req.query.eventId;
+async function handler(req, res) {
+  const eventId = req.query.eventId;
 
-    if (req.method === 'POST') {
-        const { email, name, text } = req.body;
+  if (req.method === "POST") {
+    const { email, name, text } = req.body;
 
-        if (!email.includes('@') || !name || name.trim() === '' || !text || text.trim() === '') {
-            res.status(422).json({ message: 'Invalid data!'});
-            return
-        }
-        console.log(name,email,text)
-
-        const newComment = {
-            id: new Date().toISOString(),
-            email,
-            name,
-            text
-        }
-        console.log(newComment)
-        res.status(201).json({ message: 'Added comment', comment: newComment})
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid data!" });
+      return;
     }
 
-    if (req.method === 'GET') {
-        const dummyData = [
-            { id: 'c1', name: 'Max', text: 'A first comment!'},
-            { id: 'c2', name: 'Celine', text: 'A second comment!'}
-        ];
+    const newComment = {
+      email,
+      name,
+      text,
+      eventId,
+    };
 
-        res.status(200).json({ comments: dummyData })
-    }
+    const db = await mongoDb("events");
+    const dbConnection = db.db();
+
+    const result = await dbConnection
+      .collection("comments")
+      .insertOne(newComment);
+
+    newComment.id = result.insertedId;
+
+    res.status(201).json({ message: "Added comment", comment: newComment });
+
+    db.close();
+  }
+
+  if (req.method === "GET") {
+    const db = await mongoDb("events");
+    const dbConnection = db.db();
+
+    const documents = await dbConnection
+      .collection("comments")
+      .find()
+      .sort({ _id: -1 })
+      .toArray();
+
+    const dummyData = [
+      { id: "c1", name: "Max", text: "A first comment!" },
+      { id: "c2", name: "Celine", text: "A second comment!" },
+    ];
+
+    res.status(200).json({ comments: documents });
+
+    db.close();
+  }
 }
 
 export default handler;
